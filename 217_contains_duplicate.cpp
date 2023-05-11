670ada9752d1d3c459b5b23393110a7fc5eee01d
SiHyun Im <moon990519@gmail.com>
2023-05-10 21:44:04 -0400

    solved

--- 217_contains_duplicate.cpp
+++ 217_contains_duplicate.cpp
@@ -62,8 +62,52 @@
         Time complexity is O(n log n)
         Space complexity is O(n)
     */
+    // we are going to use the python method... for the ease of algorithm
+    hash = {} 
+    // key -> element in the nums array, value -> occurence frequency
+    
+    //initialize this hash map first
+    for num in nums:
+        hash[num] = 1  // initilize the frequency as 1 
+    
+    for num in nums:
 
+        freq = hash[num]
+        //end case where we end the function if the frequency is more than 1 
+        if freq > 1:
+            return True
+        //if not, we will increment the frequency by 1 
+        hash[num] += 1 
+        // becareful when you freq = hash[num] this frequency will get affected which results in
+        // undesirable result ! freq will be keep 1 ...! 
+    //for the default case.  we are ending the fucntion as False 
+    return False
    
 
+   /* revision of the auto variable in c++ 
+   
+    auto vs auto & 
+
+    auto -> when you need a local copy. This will never produce reference.
+    The copy constructor must exist, but it might not get called due to the
+    copy elision opitmization
+
+    auto && when you don't care if the object is local or not.
+    This will always produce a reference, but if the initializer is a 
+    temporary (eg the function returns by value), it will behave essentially
+    like the own local object.
+
+    Also, this does not guarantee that the object will be modifiable.
+    const cobject or reference it will deduce const. 
+
+    auto& and auto & is a little more specific .
+    
+    auto& guarantees that you are sharing the variable with sth else.
+    It is always a reference and never to a temporary.
+
+    auto const & is like auto &&, but provide read-only access.
+   
+   */
+    
 
    
